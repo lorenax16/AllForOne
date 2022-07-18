a62e4bfec774a5da4eda4cfd5c08c75572f8bfae
lorena olivares <lorena.olivares.perez16@gmail.com>
2022-07-18 18:38:59 -0300

    requisitos 11 e 12

---
+++ desafio11.sql
@@ -0,0 +1,1 @@
+SELECT notes FROM purchase_orders where notes BETWEEN 'Purchase generated based on Order #30' and 'Purchase generated based on Order #39';
---
+++ desafio12.sql
@@ -0,0 +1,1 @@
+SELECT submitted_date from purchase_orders where date(submitted_date) = "2006-04-26";
